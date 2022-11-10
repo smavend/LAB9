24f22c2ba520a41032b241da0c49dd71f5e57220
amanr <amanrique068@gmail.com>
2022-11-09 20:37:52 -0500

    daoSElecciones

--- LAB9_IWEB_BASE/src/main/java/com/example/lab9_base/Dao/DaoPartidos.java
+++ LAB9_IWEB_BASE/src/main/java/com/example/lab9_base/Dao/DaoPartidos.java
@@ -6,35 +6,30 @@ import com.example.lab9_base.Bean.Seleccion;
 import java.sql.*;
 import java.util.ArrayList;
 
-public class DaoPartidos {
+public class DaoPartidos extends DaoBase {
     public ArrayList<Partido> listaDePartidos() {
 
         ArrayList<Partido> partidos = new ArrayList<>();
         Partido partido;
-        try {
-            String user = "root";
-            String passw = "root";
-            String url = "jdbc:mysql://localhost:3306/lab9";
-            Class.forName("com.mysql.cj.jdbc.Driver");
 
-            Connection connection = DriverManager.getConnection(url, user, passw);
-            Statement statement = connection.createStatement();
-            String sql = "select * from heroe";
-            ResultSet rs = statement.executeQuery(sql);
+        String sql = "select * from heroe";
+        try (Connection connection = this.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet rs = statement.executeQuery(sql)){
             while (rs.next()) {
-                partido = new Partido();
-                partido.setNumeroJornada(rs.getInt("numeroJornada"));
-                partido.setFecha(rs.getString("fecha"));
-                Seleccion seleccionLocal = new Seleccion();
-                seleccionLocal.setIdSeleccion(rs.getInt("seleccionLocal"));
-                partido.setSeleccionLocal();
-                heroesList.add(newHero);
+            partido = new Partido();
+            partido.setNumeroJornada(rs.getInt("numeroJornada"));
+            partido.setFecha(rs.getString("fecha"));
+            Seleccion seleccionLocal = new Seleccion();
+            seleccionLocal.setIdSeleccion(rs.getInt("seleccionLocal"));
+            partido.setSeleccionLocal();
+            heroesList.add(newHero);
             }
 
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
-        return heroesList;
+
         return partidos;
     }
 
--- LAB9_IWEB_BASE/src/main/java/com/example/lab9_base/Dao/DaoSelecciones.java
+++ LAB9_IWEB_BASE/src/main/java/com/example/lab9_base/Dao/DaoSelecciones.java
@@ -2,9 +2,10 @@ package com.example.lab9_base.Dao;
 
 import com.example.lab9_base.Bean.Seleccion;
 
+import java.sql.*;
 import java.util.ArrayList;
 
-public class DaoSelecciones {
+public class DaoSelecciones extends DaoBase {
     public ArrayList<Seleccion> listarSelecciones() {
 
         ArrayList<Seleccion> selecciones = new ArrayList<>();
@@ -13,6 +14,23 @@ public class DaoSelecciones {
         */
         return selecciones;
     }
-    public Strin
+    public String obtenerNameId (int idSeleccion){
+        Seleccion seleccion;
+        String nombre = null;
+        String sql = "SELECT nombre FROM seleccion WHERE idSeleccion = ?";
+        try(Connection connection = this.getConnection();
+            PreparedStatement pstmt = connection.prepareStatement(sql)){
+            pstmt.setInt(1,idSeleccion);
+            try (ResultSet rs = pstmt.executeQuery()){
+                if(rs.next()){
+                    nombre = rs.getString(1);
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return nombre;
+    }
 
 }
