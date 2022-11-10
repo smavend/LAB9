25c8ddda78cf15faa67e400e96a167d3cd96bec4
uwu <si@pucp.edu.pe>
2022-11-09 20:37:29 -0500

    Daoarbitro

--- LAB9_IWEB_BASE/src/main/java/com/example/lab9_base/Dao/DaoArbitros.java
+++ LAB9_IWEB_BASE/src/main/java/com/example/lab9_base/Dao/DaoArbitros.java
@@ -3,15 +3,41 @@ package com.example.lab9_base.Dao;
 import com.example.lab9_base.Bean.Arbitro;
 
 import java.util.ArrayList;
+import java.sql.*;
 
-public class DaoArbitros {
+public class DaoArbitros extends DaoBase {
+
+
+
+
     public ArrayList<Arbitro> listarArbitros() {
         ArrayList<Arbitro> arbitros = new ArrayList<>();
-        /*
-        Inserte su código aquí
-        */
+        String sql = "SELECT * FROM lab9.arbitro";
+
+        try (Connection connection = this.getConnection();
+             Statement stm = connection.createStatement();
+             ResultSet rs = stm.executeQuery(sql)) {
+
+
+            while(rs.next()){
+                Arbitro arbitro = new Arbitro();
+                arbitro.setIdArbitro(rs.getInt(1));
+                arbitro.setNombre(rs.getString(2));
+                arbitro.setPais(rs.getString(3));
+
+                arbitros.add(arbitro);
+            }
+        } catch (SQLException throwables){
+            throwables.printStackTrace();
+        }
+        
         return arbitros;
     }
+
+
+
+
+
 
     public void crearArbitro(Arbitro arbitro) {
         /*
